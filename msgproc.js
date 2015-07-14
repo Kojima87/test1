0dee8230a8f48d7d2de3fdfae89bc0535e43b6f9
Kojima87 <taka87.itmag@gmail.com>
2015-07-14 21:26:21 +0900

    ファイル名を変更したのみで未修正です。

---
+++ msgproc.js
@@ -0,0 +1,11 @@
+    <script type="text/javascript">
+
+    function submitChk () {
+        /* 確認ダイアログ表示 */
+        return confirm ("よろしいですか？");
+ //       var flag = confirm ("よろしいですか？");
+        /* send_flg が TRUEなら送信、FALSEなら送信しない */
+ //       return flag;
+    }
+
+    </script>
